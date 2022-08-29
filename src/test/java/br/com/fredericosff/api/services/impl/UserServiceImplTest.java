febdb801084d86a97d45642f39fcefa8beefb7c6
fredericoffs <fredericoffs@live.com>
2022-08-29 19:30:55 -0300

    UserService findById Not found Exception test

---
+++ src/test/java/br/com/fredericosff/api/services/impl/UserServiceImplTest.java
@@ -0,0 +1,96 @@
+package br.com.fredericosff.api.services.impl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.anyInt;
+import static org.mockito.Mockito.when;
+
+import br.com.fredericosff.api.domain.Users;
+import br.com.fredericosff.api.domain.dto.UsersDTO;
+import br.com.fredericosff.api.repositories.UserRepository;
+import br.com.fredericosff.api.services.exceptions.ObjectNotFoundException;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceImplTest {
+
+  public static final Integer ID = 1;
+  public static final String NAME = "user1";
+  public static final String EMAIL = "user1@mail.com";
+  public static final String PASSWORD = "123";
+  public static final String USER_NOT_FOUND = "User not found.";
+
+  @InjectMocks
+  private UserServiceImpl service;
+
+  @Mock
+  private UserRepository repository;
+
+  @Mock
+  private ModelMapper mapper;
+
+  private Users users;
+  private UsersDTO usersDTO;
+  private Optional<Users> optionalUsers;
+
+  @BeforeEach
+  void setUp() {
+    MockitoAnnotations.openMocks(this);
+    startUser();
+  }
+
+  @Test
+  void whenFindByIdThenReturnAnUser() {
+    when(repository.findById(anyInt())).thenReturn(optionalUsers);
+
+    Users response = service.findById(ID);
+
+    assertNotNull(response);
+
+    assertEquals(Users.class, response.getClass());
+    assertEquals(ID, response.getId());
+    assertEquals(NAME, response.getName());
+    assertEquals(EMAIL, response.getEmail());
+  }
+
+  @Test
+  void whenFindByIdThenReturnAnUserNotFoundException() {
+    when(repository.findById(anyInt())).thenThrow(new ObjectNotFoundException(USER_NOT_FOUND));
+
+    try{
+      service.findById(ID);
+    } catch (Exception ex) {
+      assertEquals(ObjectNotFoundException.class, ex.getClass());
+      assertEquals(USER_NOT_FOUND, ex.getMessage());
+    }
+  }
+
+  @Test
+  void findAll() {
+  }
+
+  @Test
+  void create() {
+  }
+
+  @Test
+  void update() {
+  }
+
+  @Test
+  void delete() {
+  }
+
+  private void startUser() {
+    users = new Users(ID, NAME, EMAIL, PASSWORD);
+    usersDTO = new UsersDTO(ID, NAME, EMAIL, PASSWORD);
+    optionalUsers = Optional.of(new Users(ID, NAME, EMAIL, PASSWORD));
+  }
+}
